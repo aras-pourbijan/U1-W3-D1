301e4c39fcb36af5892c2a4e097ce1484962dae3
Aras Pourbijan <arasmidos@gmail.com>
2022-09-12 16:41:08 +0200

    done

---
+++ js/naming.js
@@ -0,0 +1,21 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+
+
+var petPreferito = 'criceto';
+var PetPreferitouno = 'coniglio';
+console.log(PetPreferitouno);
+
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2 + petPreferito);
+var mounth = Number(12);
+console.log(mounth);
+console.log("Il mio JavaScript ha finito di eseguire!")
